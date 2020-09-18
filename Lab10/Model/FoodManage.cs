da789e21e04745267dad07ac9ffbc4c85bb1b5e9
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-09-18 16:05:36 +0700

    first commit

---
+++ Lab10/Model/FoodManage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Model
+{
+    public class FoodManage
+    {
+        public static void GetNews(string Category, ObservableCollection<NewsFood> newsFoods)
+        {
+            var allItems = GetNewsFoods();
+            var filteredNewsFoods = allItems.Where(p => p.Category == Category).ToList();
+            newsFoods.Clear();
+            filteredNewsFoods.ForEach(p => newsFoods.Add(p));
+        }
+        private static List<NewsFood> GetNewsFoods()
+        {
+            var foods = new List<NewsFood>();
+
+            foods.Add(new NewsFood() {Category="Food", Name = "Hamburger1" , Price = "4.5$", Image="Assets/ham1.jpg"  });
+            foods.Add(new NewsFood() { Category = "Food", Name = "Hamburger2", Price = "4.5$", Image = "Assets/ham2.jpg" });
+            foods.Add(new NewsFood() { Category = "Food", Name = "Hamburger3", Price = "4.5$", Image = "Assets/ham3.jpg" });
+            foods.Add(new NewsFood() { Category = "Food", Name = "Hamburger4", Price = "4.5$", Image = "Assets/ham4.jpg" });
+            foods.Add(new NewsFood() { Category = "Food", Name = "Hamburger5", Price = "4.5$", Image = "Assets/ham5.jpg" });
+
+            foods.Add(new NewsFood() { Category = "Drink", Name = "Sinh tố xoài", Price = "4.5$", Image = "Assets/st1.jpg" });
+            foods.Add(new NewsFood() { Category = "Drink", Name = "Sinh tố dâu", Price = "4.5$", Image = "Assets/st2.jpg" });
+            foods.Add(new NewsFood() { Category = "Drink", Name = "Sinh tố bơ", Price = "4.5$", Image = "Assets/st3.jpg" });
+            foods.Add(new NewsFood() { Category = "Drink", Name = "Sinh tố cam", Price = "4.5$", Image = "Assets/st4.jpg" });
+            foods.Add(new NewsFood() { Category = "Drink", Name = "Sinh tố dưa hấu", Price = "4.5$", Image = "Assets/st5.jpg" });
+            return foods;
+        }
+    }
+}
---
+++ Pratice/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Pratice.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Pratice
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Img> Images;
+        private ObservableCollection<Product> Products;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Images = new List<Img>();
+            Images.Add(new Img { ImageName = "Product1", ImagePath = "Assets/anime1.jpg" }); ;
+            Images.Add(new Img { ImageName = "Product2", ImagePath = "Assets/anime2.png" });
+            Images.Add(new Img { ImageName = "Product3", ImagePath = "Assets/anime3.jpg" });
+            Images.Add(new Img { ImageName = "Product4", ImagePath = "Assets/anime4.jpg" });
+            Images.Add(new Img { ImageName = "Product5", ImagePath = "Assets/anime5.jpg" });
+            Images.Add(new Img { ImageName = "Product6", ImagePath = "Assets/anime6.jpg" });
+            ImageProduct.ItemsSource = Images;
+            ImageProduct.SelectedIndex = 0;
+
+            Products = new ObservableCollection<Product>();
+        }
+        private void NewProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            string imageProduct = ((Img)ImageProduct.SelectedValue).ImagePath;
+            Products.Add(new Product { ProductName = product.Text, Description = description.Text, ImageProduct = imageProduct });
+
+            product.Text = "";
+            description.Text = "";
+            ImageProduct.SelectedIndex = -1;
+
+            product.Focus(FocusState.Programmatic);
+        }
+    }
+}
