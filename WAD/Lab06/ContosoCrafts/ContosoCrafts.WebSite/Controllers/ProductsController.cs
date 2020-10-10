85161d039366b65aca51e2723470c018d9aacca3
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-10-10 10:57:24 +0700

    Lab06-ContosoCrafts

---
+++ WAD/Lab06/ContosoCrafts/ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsServices productService)
+        {
+
+        }
+
+        public JsonFileProductsServices ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+                    return ProductService.GetProducts();
+        }
+
+        //[HttpPatch]
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string productId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(productId, Rating);
+            return Ok();
+        }
+    }
+}
