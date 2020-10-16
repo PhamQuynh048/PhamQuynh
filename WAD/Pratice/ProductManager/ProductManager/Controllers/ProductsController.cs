e0451d29d3cfddf1d5b37ef26d1665645e382ae3
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-10-16 11:12:40 +0700

    Pratice

---
+++ WAD/Pratice/ProductManager/ProductManager/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Manager;
+using ProductManager.Models;
+
+namespace ProductManager.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsManager productManager)
+        {
+            this.ProductManager = productManager;
+        }
+
+        public JsonFileProductsManager ProductManager { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductManager.GetProducts();
+        }
+    }
+}
---
+++ WAD/Pratice/ProductManager/ProductManager/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ProductManager.Manager;
+using ProductManager.Models;
+
+namespace ProductManager.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public JsonFileProductsManager ProductManager;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+        JsonFileProductsManager productManager)
+        {
+            _logger = logger;
+            ProductManager = productManager;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductManager.GetProducts();
+        }
+    }
+}
