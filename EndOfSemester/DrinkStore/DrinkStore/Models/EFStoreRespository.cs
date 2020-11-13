2345e10f40390c81491bdd1a3d2f05b066711d4d
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-11-13 23:07:46 +0700

    Lab01

---
+++ EndOfSemester/DrinkStore/DrinkStore/Models/EFStoreRespository.cs
@@ -0,0 +1,19 @@
+﻿using OrderStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkStore.Models
+{
+    public class EFStoreRespository : IStoreRepository
+    {
+        private StoreDbContext context;
+        public EFStoreRespository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
---
+++ EndOfSemester/DrinkStore/DrinkStore/Models/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using DrinkStore.Models;
+using OrderStore.Models;
+
+namespace DrinkStore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Status = "New",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak1",
+                        Description = "A boat for one person",
+                        Status = "New",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak2",
+                        Description = "A boat for one person",
+                        Status = "New",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak3",
+                        Description = "A boat for one person",
+                        Status = "New",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak4",
+                        Description = "A boat for one person",
+                        Status = "New",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak5",
+                        Description = "A boat for one person",
+                        Status = "New",
+                        Price = 275
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
