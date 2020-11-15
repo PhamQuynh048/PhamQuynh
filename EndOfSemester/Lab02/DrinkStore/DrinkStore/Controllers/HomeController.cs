0706c47e7cb597b16b7e8ae23863a0de102340eb
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-11-16 00:39:02 +0700

    Lab02

---
+++ EndOfSemester/Lab02/DrinkStore/DrinkStore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DrinkStore.Models;
+using DrinkStore.Models.ViewModels;
+
+namespace DrinkStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        
+
+        public ViewResult Index(int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPErPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
+    }
+}
---
+++ EndOfSemester/Lab02/DrinkStore/DrinkStore/Models/IStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkStore.Models
+{
+    public class IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
