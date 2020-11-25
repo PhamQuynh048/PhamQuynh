2b2c2132ca413c9f5aeda9f1adbc0434310e78c4
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-11-25 23:25:01 +0700

    lab3

---
+++ EndOfSemester/Lab3/DrinksStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DrinksStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
