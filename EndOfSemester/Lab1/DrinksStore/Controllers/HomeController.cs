48f4a8d4071b6c7bf886f8ccca1dcae969540561
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-11-25 15:38:43 +0700

    Lab01

---
+++ EndOfSemester/Lab1/DrinksStore/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DrinksStore.Models;
+
+namespace DrinksStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
