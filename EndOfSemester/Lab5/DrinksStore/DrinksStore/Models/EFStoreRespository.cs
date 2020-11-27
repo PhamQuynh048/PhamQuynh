9cfee62dfdfeb18f6a52ed75a336e7c631f1627c
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2020-11-28 00:24:58 +0700

    Lab5

---
+++ EndOfSemester/Lab5/DrinksStore/DrinksStore/Models/EFStoreRespository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinksStore.Models
+{
+    public class EFStoreRespository : IStoreRepository
+    {
+        private StoreDbContext context;
+        public EFStoreRespository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
